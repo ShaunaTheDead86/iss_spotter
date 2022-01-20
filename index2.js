6b13df8adc41abc4eb69aae69a05d571909f3830
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-20 17:59:17 -0500

    adding error catch to promise chain

--- index2.js
+++ index2.js
@@ -9,4 +9,7 @@ fetchMyIP()
       const date = new Date(flyover.risetime * 1000);
       console.log(`Next pass at ${date} for ${flyover.duration} seconds!`);
     }
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
   });
